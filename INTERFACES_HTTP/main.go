f02d04b88816bc26021586dfc800d2f5dd335e65
Andi Hakim <andi.arif@stptower.com>
2018-05-31 18:24:41 +0700

    interfaces & assignment

--- INTERFACES_HTTP/main.go
+++ INTERFACES_HTTP/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	// resp, err := http.Get("http://id.techinasia.com/")
@@ -14,8 +17,22 @@ func main() {
 		os.Exit(1)
 	}
 
-	// byte slice from resp
-	// 99999 = number of element avilable
-	bs := make([]byte, 99999)
+	// // byte slice from resp
+	// // 99999 = number of element avilable
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+	// fmt.Print(resp.ContentLength)
 
+	lw := logWriter{}
+
+	// lw, sebelumnya = os.stdout
+	io.Copy(lw, resp.Body)
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Total bytes: ", len(bs))
+	return len(bs), nil
 }
